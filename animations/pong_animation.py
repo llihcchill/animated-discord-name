b08d80328e21d86cddf6ea6bfaf76c37af7de29e
llihcchill <125551072+llihcchill@users.noreply.github.com>
2024-01-31 21:43:52 +1000

    Update pong_animation.py

--- animations/pong_animation.py
+++ animations/pong_animation.py
@@ -1,5 +1,14 @@
 # this one uses up too much space for a username so it is only compatable with the about me script, though it can be adapted
 
 frames = [
-  {""}
+  {"-          -\n-          -\n|         *|\n-          -\n-          -"},
+  {"-          -\n-          -\n|          |\n-        * -\n-          -"},
+  {"-          -\n|          -\n-          |\n-          -\n-       *  -"},
+  {"-          -\n-          -\n|          |\n-       *  -\n-          -"},
+  {"-          -\n-          -\n|      *   |\n-          -\n-          -"},
+  {"-          -\n-     *    -\n|          |\n-          -\n-          -"},
+  {"-    *     -\n|          -\n-          |\n-          -\n-          -"},
+  {"|          -\n-   *      -\n-          |\n-          -\n-          -"},
+  {"-          -\n|          -\n-  *       |\n-          -\n-          -"},
+  {"-          -\n|          -\n*          |\n-          -\n-          -"},
 ]
